9dd11864c22824cfca6c7dc3ba2993908af33712
JohanClamy <clamyjohan@yahoo.fr>
2018-02-08 10:58:00 +0000

    Ajout formulaire profilutilisateur

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,7 @@ class ProfilesController < ApplicationController
     #GET vers /users/:user_id/profile/new
     def new
         #Render blank profile details form
+        @profile = Profile.new
     end
+    
 end
