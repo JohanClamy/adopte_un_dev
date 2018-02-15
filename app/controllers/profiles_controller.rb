a8c7ae73aaadc4050d960459d9d920d586879e2c
JohanClamy <clamyjohan@yahoo.fr>
2018-02-15 20:17:13 +0000

    Ajout before filtre pour limitation d'acces

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
-    
+    before_action :authenticate_user!
+    before_action :only_current_user
     
     #GET vers /users/:user_id/profile/new
     def new
@@ -47,4 +48,11 @@ class ProfilesController < ApplicationController
         def profile_params
           params.require(:profile).permit(:first_name, :avatar, :last_name, :job_title, :phone_number, :contact_email, :description)   
         end
+        
+        def only_current_user
+          @user = User.find( params[:user_id] )
+          redirect_to(root_url) unless @user == current_user
+        end
+        
+       
 end
