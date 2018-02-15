c32a4fe31609fa0048666ec5c476ccef8d1a0748
JohanClamy <clamyjohan@yahoo.fr>
2018-02-15 17:23:50 +0000

    Ajout de l'action de mise a jour de profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,14 +15,36 @@ class ProfilesController < ApplicationController
       @profile = @user.build_profile( profile_params )
      if  @profile.save
        flash[:success] = "Profil mise à jour"
-       redirect_to user_path( params[:user_id])
+       redirect_to user_path(id: params[:user_id])
       else
         render action: :new
      end
     end
     
-    private
-      def profile_params
-        params.require(:profile).permit(:first_name, :avatar, :last_name, :job_title, :phone_number, :contact_email, :description)   
+    #GET vers /users/:user_id/profile/edit
+    def edit
+      @user= User.find( params[:user_id] )
+      @profile = @user.profile
+    end
+    
+      # PATCH to /users/:user_id/profile
+      def update
+        # Retrieve that user's profile
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        # Mass assign edited profile attributes and save (update)
+        if @profile.update_attributes(profile_params)
+          flash[:success] = "Profile updated!"
+          # Redirect user to their profile page
+          redirect_to user_path(id: params[:user_id] )
+        else
+          render action: :edit
+          
+        end
       end
+      
+      private
+        def profile_params
+          params.require(:profile).permit(:first_name, :avatar, :last_name, :job_title, :phone_number, :contact_email, :description)   
+        end
 end
